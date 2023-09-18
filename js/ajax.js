57b58a5d0b6d294d3c1541c7bbffb3878d0ff4c2
LucasChanquia <lucaschanquia@gmail.com>
2023-09-18 18:11:37 -0300

    Ajax form configuration

---
+++ js/ajax.js
@@ -0,0 +1,32 @@
+const formulariosAjax = document.querySelectorAll(".FormularioAjax");
+
+function enviar_formulario_ajax(e){
+    e.preventDefault();
+
+    let enviar = confirm("Quieres enviar el formulario?");
+
+    if(enviar == true){
+        let data = new FormData(this);
+        let method = this.getAttribute("method");
+        let action = this.getAttribute("action");
+
+        let encabezados = new Headers();
+
+        let config={
+            method: method,
+            headers: encabezados,
+            mode: "cors",
+            cache: "no-cache",
+            body: data,
+        }
+
+        fetch(action,config)
+        .then(res => res.text())
+        .then(res => { alert(res)
+        });
+    }
+}
+
+formulariosAjax.forEach(formularios =>{
+    formularios.addEventListener("submit", enviar_formulario_ajax);
+});
