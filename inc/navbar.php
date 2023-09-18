cad2e56ce8e075756fd6a7cb117409243ae9964a
LucasChanquia <lucaschanquia@gmail.com>
2023-09-18 18:10:49 -0300

    Change logo and redirection

--- inc/navbar.php
+++ inc/navbar.php
@@ -1,7 +1,7 @@
 <nav class="navbar" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
-    <a class="navbar-item" href="https://bulma.io">
-      <img src="./img/pngwing.com.png" width="65" height="28">
+    <a class="navbar-item" href="?vista=home">
+      <img src="./img/mysql.png" width="65" height="28">
     </a>
 
     <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
