21295d0709930df64ea0242ed15885b8b861ba91
LucasChanquia <lucaschanquia@gmail.com>
2023-10-04 23:03:38 -0300

    Added function page of vista

--- vistas/product_list.php
+++ vistas/product_list.php
@@ -4,54 +4,32 @@
 </div>
 
 <div class="container pb-6 pt-6">
+    <?php
+    require_once "./php/main.php";
 
+    # Eliminar producto #
+    if (isset($_GET['product_id_del'])) {
+        require_once "./php/producto_eliminar.php";
+    }
 
-    <article class="media">
-        <figure class="media-left">
-            <p class="image is-64x64">
-                <img src="./img/producto.png">
-            </p>
-        </figure>
-        <div class="media-content">
-            <div class="content">
-                <p>
-                    <strong>1 - Nombre de producto</strong><br>
-                    <strong>CODIGO:</strong> 00000000, 
-                    <strong>PRECIO:</strong> $10.00, 
-                    <strong>STOCK:</strong> 21, 
-                    <strong>CATEGORIA:</strong> Nombre categoria, 
-                    <strong>REGISTRADO POR:</strong> Nombre de usuario
-                </p>
-            </div>
-            <div class="has-text-right">
-                <a href="#" class="button is-link is-rounded is-small">Imagen</a>
-                <a href="#" class="button is-success is-rounded is-small">Actualizar</a>
-                <a href="#" class="button is-danger is-rounded is-small">Eliminar</a>
-            </div>
-        </div>
-    </article>
+    if (!isset($_GET['page'])) {
+        $pagina = 1;
+    } else {
+        $pagina = (int) $_GET['page'];
+        if ($pagina <= 1) {
+            $pagina = 1;
+        }
+    }
 
+    $categoria_id = (isset($_GET['category_id'])) ? $_GET['category_id'] : 0;
 
-    <hr>
+    $pagina = limpiar_cadena($pagina);
+    $url = "index.php?vista=product_list&page="; /* <== */
+    $registros = 15;
+    $busqueda = "";
 
-
-    <p class="has-text-right">Mostrando productos <strong>1</strong> al <strong>17</strong> de un <strong>total de 17</strong></p>
-
-
-    <nav class="pagination is-centered is-rounded" role="navigation" aria-label="pagination">
-        <a class="pagination-previous" href="#">Anterior</a>
-
-        <ul class="pagination-list">
-            <li><a class="pagination-link" href="#">1</a></li>
-            <li><span class="pagination-ellipsis">&hellip;</span></li>
-            <li><a class="pagination-link is-current" href="#">2</a></li>
-            <li><a class="pagination-link" href="#">3</a></li>
-            <li><span class="pagination-ellipsis">&hellip;</span></li>
-            <li><a class="pagination-link" href="#">3</a></li>
-        </ul>
-
-        <a class="pagination-next" href="#">Siguiente</a>
-    </nav>
-
+    # Paginador categoria #
+    require_once "./php/producto_lista.php";
+    ?>
 
 </div>
